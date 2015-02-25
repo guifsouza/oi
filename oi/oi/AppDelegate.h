f3bea941b099219131cfd16718725662af9f9df6
Guilherme Ferreira de Souza <guilherme.souza94@hotmail.com>
2015-02-25 14:54:31 -0300

    oi

---
+++ oi/oi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  oi
+//
+//  Created by Guilherme Ferreira de Souza on 2/25/15.
+//  Copyright (c) 2015 Guilherme Ferreira de Souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ oi/oi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oi
+//
+//  Created by Guilherme Ferreira de Souza on 2/25/15.
+//  Copyright (c) 2015 Guilherme Ferreira de Souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
